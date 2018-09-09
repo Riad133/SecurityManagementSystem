d8c94b6da237460f34a7e0d4bba6d328bf164f5d
riad133 <riad.nazrul@gmail.com>
2018-09-09 17:41:22 +0600

    Add Spring Security Initializer and Security Configuration.Then sync username,passwords,roles ininially.

---
+++ src/main/java/com/riad/springsecurity/demo/security/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.riad.springsecurity.demo.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+              extends AbstractSecurityWebApplicationInitializer {
+
+}
