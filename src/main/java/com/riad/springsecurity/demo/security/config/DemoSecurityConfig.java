58cfdcfff5fb1ce5073e2694315f87f84c0025b0
riad133 <riad.nazrul@gmail.com>
2018-09-19 02:08:58 +0600

    Add DB support to POM file and define JDBC properties,Datasouce in Srping configaration and Update spring security Configuration to use JDBC

--- src/main/java/com/riad/springsecurity/demo/security/config/DemoSecurityConfig.java
+++ src/main/java/com/riad/springsecurity/demo/security/config/DemoSecurityConfig.java
@@ -1,35 +1,36 @@
 package com.riad.springsecurity.demo.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		//super.configure(auth);
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("jhon").password("test123").roles("EMPLOYEE"))
-		.withUser(users.username("mary").password("test123").roles("MANAGER"));
+		auth.jdbcAuthentication()
+		.dataSource(securityDataSource);
 				
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http.authorizeRequests()
-		    
-		    .antMatchers("/leaders/**").hasAnyRole("MANAGER")
+		     http.authorizeRequests()
+		    .antMatchers("/").hasAnyRole("EMPLOYEE")
+		    .antMatchers("/leaders/**").hasAnyRole("MANAGER","EMPLOYEE")
 		    .antMatchers("/employees/**").hasAnyRole("EMPLOYEE")
 		    .and()
 		    .exceptionHandling().accessDeniedPage("/access-denied")
